c83463e1b0de5346abd13a5a5cd39c2ed9d38023
mateuszrosa <mateusz.rosa@gmail.com>
2019-05-24 16:47:42 +0200

    canvas width and height

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 1000;
+canvas.height = 500;
+
+const cw = canvas.width;
+const ch = canvas.height;
