a1b7c2c0e896c4785efd81ef5b241046467b48a7
mateuszrosa <mateusz.rosa@gmail.com>
2019-05-26 15:40:41 +0200

    change height of the canvas

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
 canvas.width = 1000;
-canvas.height = 500;
+canvas.height = 800;
 
 const cw = canvas.width;
 const ch = canvas.height;
