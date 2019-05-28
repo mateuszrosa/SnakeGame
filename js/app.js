a55b85dc1144424aa601e97f308e7b55b948671d
mateuszrosa <mateusz.rosa@gmail.com>
2019-05-28 15:45:14 +0200

    reset game

--- js/app.js
+++ js/app.js
@@ -1,40 +1,5 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.width = 1000;
-canvas.height = 800;
-
-const cw = canvas.width;
-const ch = canvas.height;
-
-const snakeWidth = 75;
-const snakeHeight = 20;
-
-let snakeWidthStart = 500;
-let snakeHeightStart = 200;
-
-const table = () => {
-    ctx.fillStyle = "#0000ff";
-    ctx.fillRect(0, 0, cw, ch);
-};
-
-const snake = () => {
-    ctx.fillStyle = "black";
-    ctx.fillRect(snakeWidthStart, snakeHeightStart, snakeWidth, snakeHeight);
-}
-
-const snakeMove = () => {
-    snakeWidthStart += 10;
-}
-
-table();
-snake();
-
-setInterval(() => {
-    if (snakeWidthStart >= cw) {
-        snakeWidthStart = 0 - snakeWidth;
-    }
-    table();
-    snakeMove();
-    snake();
-}, 200);
+canvas.width = 300;
+canvas.height = 300;
