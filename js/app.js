f5ec954e6011f77ca633d62e2fe7788fdc5df97c
mateuszrosa <mateusz.rosa@gmail.com>
2019-05-28 18:50:58 +0200

    drawning snake part by part

--- js/app.js
+++ js/app.js
@@ -9,4 +9,6 @@ let snake = [{ x: 150, y: 150 }, { x: 140, y: 150 }, { x: 130, y: 150 }, { x: 12
 const drawSnakePart = snakePart => {
     ctx.fillStyle = 'lightgreen';
     ctx.strokestyle = 'darkgreen';
+    ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+    ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
 }
