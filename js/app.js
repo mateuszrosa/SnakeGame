9bf676b92389481117618b34541212b06d04eecf
mateuszrosa <mateusz.rosa@gmail.com>
2019-05-28 18:45:37 +0200

    snake coordinates

--- js/app.js
+++ js/app.js
@@ -3,3 +3,5 @@ const ctx = canvas.getContext('2d');
 
 canvas.width = 300;
 canvas.height = 300;
+
+let snake = [{ x: 150, y: 150 }, { x: 140, y: 150 }, { x: 130, y: 150 }, { x: 120, y: 150 }, { x: 110, y: 150 }, ];
