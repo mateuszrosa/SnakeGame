550569bbd29748c250fc79f0f3a68d354a3e3b8f
mateuszrosa <mateusz.rosa@gmail.com>
2019-05-29 16:31:05 +0200

    advance snake

--- js/app.js
+++ js/app.js
@@ -4,7 +4,13 @@ const ctx = canvas.getContext('2d');
 canvas.width = 300;
 canvas.height = 300;
 
-let snake = [{ x: 150, y: 150 }, { x: 140, y: 150 }, { x: 130, y: 150 }, { x: 120, y: 150 }, { x: 110, y: 150 }, ];
+let snake = [
+    { x: 150, y: 150 },
+    { x: 140, y: 150 },
+    { x: 130, y: 150 },
+    { x: 120, y: 150 },
+    { x: 110, y: 150 },
+];
 
 const drawSnakePart = snakePart => {
     ctx.fillStyle = 'lightgreen';
@@ -17,4 +23,11 @@ const drawSnake = () => {
     snake.forEach(drawSnakePart);
 }
 
+const advanceSnake = () => {
+    const head = { x: snake[0].x + 0, y: snake[0].y - 10 };
+    snake.unshift(head);
+    snake.pop();
+}
+
+advanceSnake();
 drawSnake();
