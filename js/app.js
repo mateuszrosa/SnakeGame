a8d044aa8bcd9dbdc8331e2a401ac4df84bd9b3c
mateuszrosa <mateusz.rosa@gmail.com>
2019-05-30 18:50:15 +0200

    moving snake by keydown

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@ const ctx = canvas.getContext('2d');
 canvas.width = 300;
 canvas.height = 300;
 
+let dx = 10;
+let dy = 0;
+
 let snake = [
     { x: 150, y: 150 },
     { x: 140, y: 150 },
@@ -27,8 +30,6 @@ const advanceSnake = () => {
     if (snake[0].x == 300) {
         snake[0].x = 0;
     }
-    dx = 10;
-    dy = 0;
     const head = { x: snake[0].x + dx, y: snake[0].y + dy };
     snake.unshift(head);
     snake.pop();
@@ -51,5 +52,38 @@ const main = () => {
         main();
     }, 100)
 };
+
+
+const changeDirection = event => {
+    const left_key = 37;
+    const right_key = 39;
+    const up_key = 38;
+    const down_key = 40;
+    const keyPressed = event.keyCode;
+    const goingUp = dy === -10;
+    const goingDown = dy === 10;
+    const goingRight = dx === 10;
+    const goingLeft = dx === -10;
+    if (keyPressed === left_key && !goingRight) {
+        dx = -10;
+        dy = 0;
+        console.log(dx, dy);
+    }
+    if (keyPressed === up_key && !goingDown) {
+        dx = 0;
+        dy = -10;
+        console.log(dx, dy);
+    }
+    if (keyPressed === right_key && !goingLeft) {
+        dx = 10;
+        dy = 0;
+    }
+    if (keyPressed === down_key && !goingUp) {
+        dx = 0;
+        dy = 10;
+    }
+}
+
+document.addEventListener('keydown', changeDirection);
 
 main();
