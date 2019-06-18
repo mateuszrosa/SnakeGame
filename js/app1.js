45294f74d5395a82112c62aac850a8497c94de2c
mateuszrosa <mateusz.rosa@gmail.com>
2019-06-18 16:58:20 +0200

    oop

--- js/app1.js
+++ js/app1.js
@@ -2,8 +2,22 @@ class Snake {
     constructor() {
         this.canvas = document.querySelector('canvas');
         this.ctx = this.canvas.getContext('2d');
+        this.canvas.width = 300;
+        this.canvas.height = 300;
+        this.dx = 10;
+        this.dy = 0;
+        this.foodX;
+        this.foodY;
+        this.changingDirection = false;
+        this.score = 0;
 
-        console.log(this.canvas);
+        this.snake = [
+            { x: 150, y: 150 },
+            { x: 140, y: 150 },
+            { x: 130, y: 150 },
+            { x: 120, y: 150 },
+            { x: 110, y: 150 },
+        ];
     }
 }
 
