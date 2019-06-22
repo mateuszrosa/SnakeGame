43138b1fe2f04ae79690360ffffb881566208e25
mateuszrosa <mateusz.rosa@gmail.com>
2019-06-22 17:28:24 +0200

    alert after lose

--- js/app1.js
+++ js/app1.js
@@ -59,7 +59,16 @@ class Snake {
             this.advanceSnake();
             this.drawFood();
             this.drawSnake();
-            if (this.endGame()) return;
+            if (this.endGame()) {
+                alert("Lose");
+                this.snake = [
+                    { x: 150, y: 150 },
+                    { x: 140, y: 150 },
+                    { x: 130, y: 150 },
+                    { x: 120, y: 150 },
+                    { x: 110, y: 150 },
+                ];
+            }
             this.main();
         }, 100)
     }
